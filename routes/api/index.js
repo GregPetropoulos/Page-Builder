11cebfd45ccc4af540ae7c19e462ca9c6da00547
pdsan97 <77399330+pdsan97@users.noreply.github.com>
2021-06-26 09:44:02 -0400

    route fix
    fixed some routes hopefully

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const pageRoutes = require ('./pageRoutes');
-
-// API Routes
-router.use('/users', userRoutes);
-router.use('/page', pageRoutes);
-
-module.exports = router;
