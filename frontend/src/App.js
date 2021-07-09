a3b2c6dc331bc252f37b66316b3c4779eefadfa9
pdsan97 <>
2021-07-09 11:59:22 +0300

    Add stats page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuthContext } from './context/AuthContext';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { TemplateEditor } from './components/TemplateEditor';
 import { TemplatePage } from './pages/TemplatesPage';
-import { ProjectsPage } from './pages/ProjectsPage';
 import { HomePage } from './pages/HomePage';
 import { PageView } from './pages/PageView';
 import { LandingPage } from './pages/LandingPage';
@@ -13,33 +12,66 @@ import { templates } from './templates/templates';
 import { ProfilePage } from './pages/ProfilePage';
 import { Footer } from './components/Footer';
 import { PrivateRoute } from './PrivateRoute';
+import { Statistics } from './pages/Statistics';
+import axios from 'axios';
+
+const visitPage = async () => {
+	await axios.post('/api/visit', {});
+};
 
 function App() {
 	const { currentUser, signOut } = useContext(AuthContext);
+
+	useEffect(() => {
+		visitPage();
+	}, []);
+
 	return (
 		<div className="bg-gray-200">
-			<Navbar currentUserProp={currentUser}/>
+			<Navbar currentUserProp={currentUser} />
 			<BrowserRouter>
 				<Switch>
-
-				
 					<PrivateRoute path="/templates">
-						<TemplatePage configs={templates} currentUserProp={currentUser} signOutFunc={signOut} />
+						<TemplatePage
+							configs={templates}
+							currentUserProp={currentUser}
+							signOutFunc={signOut}
+						/>
+					</PrivateRoute>
+					<PrivateRoute path="/statistics">
+						<Statistics />
 					</PrivateRoute>
 					{/* <PrivateRoute path="/projects">
 						<ProjectsPage/>
 					</PrivateRoute> */}
 					<PrivateRoute path="/create/:id">
-						<TemplateEditor configs={templates} currentUserProp={currentUser} signOutFunc={signOut} />
+						<TemplateEditor
+							configs={templates}
+							currentUserProp={currentUser}
+							signOutFunc={signOut}
+						/>
 					</PrivateRoute>
 					<PrivateRoute path="/page/:id">
-						<PageView currentUserProp={currentUser} signOutFunc={signOut}/>
+						<PageView
+							currentUserProp={currentUser}
+							signOutFunc={signOut}
+						/>
 					</PrivateRoute>
 					<PrivateRoute path="/profilepage">
-						<ProfilePage currentUserProp={currentUser} signOutFunc={signOut}/>
+						<ProfilePage
+							currentUserProp={currentUser}
+							signOutFunc={signOut}
+						/>
 					</PrivateRoute>
 					<Route path="/">
-						{ !currentUser ? ( <LandingPage />): <HomePage currentUserProp={currentUser} signOutFunc={signOut}/> }
+						{!currentUser ? (
+							<LandingPage />
+						) : (
+							<HomePage
+								currentUserProp={currentUser}
+								signOutFunc={signOut}
+							/>
+						)}
 					</Route>
 				</Switch>
 			</BrowserRouter>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,57 +1,66 @@
 import logopb from '../images/logopb.png';
 
-export const Navbar = (props) => {
-  const { currentUserProp } = props;
-  return (
-    <div className='p-3'>
-      <div className='flex flex-wrap navbar mb-2 shadow-lg bg-gray-800 text-neutral-content rounded-xl'>
-        <div className='flex-none px-2 mx-2 align-middle'>
-          <span className='text-lg font-bold'>
-            <img src={logopb} alt='logopb' className='w-20 h-20' />
-          </span>
-        </div>
-        <div className='flex'>
-          {currentUserProp ? (
-            <div className='items-stretch hidden lg:flex content-center'>
-              <a
-                className='flex items-center align-middle text-sm uppercase text-indigo-50 font-black'
-                href='/'
-              >
-                Home
-              </a>
-              <a
-                className='btn btn-ghost btn-sm rounded-btn px-3 py-2 flex items-center text-sm uppercase text-indigo-50 font-black'
-                href='/profilepage'
-              >
-                Profile
-              </a>
-              <a
-                className='btn btn-ghost btn-sm rounded-btn px-3 py-2 flex items-center text-sm uppercase text-indigo-50 font-black'
-                href='/templates'
-              >
-                Templates
-              </a>
-            </div>
-          ) : (
-            ''
-          )}
-        </div>
-        {currentUserProp ? (
-          <div className='flex float-right'>
-            <div className='avatar'>
-              <div className='rounded-xl'>
-                <img
-                  src='https://i.pravatar.cc/500?img=32'
-                  className='rounded-full w-10 h-10 m-1 float-right'
-                  alt='avatar'
-                />
-              </div>
-            </div>
-          </div>
-        ) : (
-          ''
-        )}
-      </div>
-    </div>
-  );
+export const Navbar = props => {
+	const { currentUserProp } = props;
+	return (
+		<div className="p-3">
+			<div className="flex flex-wrap navbar mb-2 shadow-lg bg-gray-800 text-neutral-content rounded-xl">
+				<div className="flex-none px-2 mx-2 align-middle">
+					<span className="text-lg font-bold">
+						<img src={logopb} alt="logopb" className="w-20 h-20" />
+					</span>
+				</div>
+				<div className="flex">
+					{currentUserProp ? (
+						<div className="items-stretch hidden lg:flex content-center">
+							<a
+								className="flex items-center align-middle text-sm uppercase text-indigo-50 font-black"
+								href="/"
+							>
+								Home
+							</a>
+							<a
+								className="btn btn-ghost btn-sm rounded-btn px-3 py-2 flex items-center text-sm uppercase text-indigo-50 font-black"
+								href="/profilepage"
+							>
+								Profile
+							</a>
+							<a
+								className="btn btn-ghost btn-sm rounded-btn px-3 py-2 flex items-center text-sm uppercase text-indigo-50 font-black"
+								href="/templates"
+							>
+								Templates
+							</a>
+							<a
+								className="btn btn-ghost btn-sm rounded-btn px-3 py-2 flex items-center text-sm uppercase text-indigo-50 font-black"
+								href="/statistics"
+							>
+								Statistics
+							</a>
+						</div>
+					) : (
+						''
+					)}
+				</div>
+				{currentUserProp ? (
+					<div
+						className="flex float-right"
+						style={{ alignItems: 'center' }}
+					>
+						<div className="avatar">
+							<div className="rounded-xl">
+								<img
+									src="https://i.pravatar.cc/500?img=32"
+									className="rounded-full w-10 h-10 m-1 float-right"
+									alt="avatar"
+								/>
+							</div>
+						</div>
+					</div>
+				) : (
+					''
+				)}
+			</div>
+		</div>
+	);
 };
